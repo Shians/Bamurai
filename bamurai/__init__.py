8f9c933dd2e316e4e503ae6e3b66d5c36e985fa8
shians <registertonysu@gmail.com>
2025-02-10 20:53:19 +1100

    Cleaned up exports

--- bamurai/__init__.py
+++ bamurai/__init__.py
@@ -1,11 +1,9 @@
 from .core import (
     Read,
     count_reads,
-    qual_to_fastq_numpy,
     parse_reads,
-    keep_n_bases,
     split_read,
     calculate_split
 )
 
-__version__ = "1.0.0"
+__version__ = "0.1.0"
