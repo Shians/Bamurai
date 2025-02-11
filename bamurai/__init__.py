df2e2cbea42df5da2b5cf7470cd7629691a08a32
shians <registertonysu@gmail.com>
2025-02-11 13:23:35 +1100

    Refactored init

--- bamurai/__init__.py
+++ bamurai/__init__.py
@@ -1,18 +1,6 @@
-
-from .core import (
-    Read,
-    parse_reads,
-    split_read,
-    calculate_split,
-    read_version
-)
-
-from .stats import (
-    file_stats,
-    is_fastq,
-    calc_n50,
-    bam_file_stats,
-    fastq_file_stats
-)
+from .core import *
+from .stats import *
+from .split import *
+from .divide import *
 
 __version__ = read_version()
