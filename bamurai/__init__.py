7d13ad64ad0316195d33028494a155fb03888572
shians <registertonysu@gmail.com>
2025-02-11 11:59:12 +1100

    Updated exports

--- bamurai/__init__.py
+++ bamurai/__init__.py
@@ -1,9 +1,16 @@
 from .core import (
     Read,
-    count_reads,
     parse_reads,
     split_read,
     calculate_split
+)
+
+from .stats import (
+    file_stats,
+    is_fastq,
+    calc_n50,
+    bam_file_stats,
+    fastq_file_stats
 )
 
 __version__ = "0.1.0"
