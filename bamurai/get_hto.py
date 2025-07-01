61ad392142b030d779df88d2384c0314e11c5347
shians <registertonysu@gmail.com>
2025-07-01 16:57:34 +1000

    Simplified reading

--- bamurai/get_hto.py
+++ bamurai/get_hto.py
@@ -17,18 +17,20 @@ def get_hto(args):
     r2_open = gzip.open if r2_file.endswith('.gz') else open
 
     with r1_open(r1_file, 'rt') as f_r1, r2_open(r2_file, 'rt') as f_r2:
-        with open(output_file, 'w', encoding = 'utf-8') as out_f:
-            out_f.write("read_name\tcell_barcode\tumi\thto")
-            for r1_lines, r2_lines in zip_longest(*[f_r1]*4, *[f_r2]*4):
-                if not r1_lines or not r2_lines:
+        with open(output_file, 'w', encoding='utf-8') as out_f:
+            out_f.write("read_name\tcell_barcode\tumi\thto\n")
+            while True:
+                r1_lines = [f_r1.readline() for _ in range(4)]
+                r2_lines = [f_r2.readline() for _ in range(4)]
+                if not r1_lines[0] or not r2_lines[0]:
                     break
 
+                read_name = r1_lines[0].strip().split()[0][1:]  # Remove '@' and take first word
                 r1_seq = r1_lines[1].strip()
                 r2_seq = r2_lines[1].strip()
-                read_name = r1_lines[0].strip().split()[0][1:]  # Remove '@' and take first word
 
                 cell_barcode = r1_seq[:barcode_len]
                 umi = r1_seq[barcode_len:(barcode_len + umi_len)]
                 hto = r2_seq[hashtag_left_buffer:(hashtag_left_buffer + hashtag_len)]
 
-                out_f.write(f"{read_name}\t{cell_barcode}\t{umi}\t{hto}")
+                out_f.write(f"{read_name}\t{cell_barcode}\t{umi}\t{hto}\n")
